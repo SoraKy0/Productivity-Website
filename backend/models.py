1005a4b5e2371a23f07b452bd64c8b9352eb961a
SoraKy0 <154877077+SoraKy0@users.noreply.github.com>
2026-02-09 03:12:04 +0000

    implement model inheritance (Base, Create, Public) for data integrity

--- backend/models.py
+++ backend/models.py
@@ -9,10 +9,23 @@ Goals:
 from sqlmodel import SQLModel, Field
 from datetime import datetime, timezone
 
-class TODO(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    title: str
+# Contains the fields that all TODO models will have. To avoid repeating code.
+class TODOBase(SQLModel):
+    title: str = Field(max_length=100)
     description: str | None = Field(default=None, max_length=600)
-    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc)) # Automatically set the timestamp to the current time when a new TODO is created
     position: int = Field(default=0) # Pos of TODO in list.
     priority: int = Field(default=0) # There will be 3 levels of priority. (0 = No priority, 1 = Low, 2 = Medium, 3 = High)
+
+# This is what the user sends to the API when creating a new TODO.
+class TODOCreate(TODOBase):
+    pass
+
+# This is what we send to the user screen. It gurantees id and timestamp will be included in the data to the frontend.
+class TODOPublic(TODOBase):
+    id: int
+    timestamp: datetime
+
+# This is the actual SQL table in the database.
+class TODO(TODOBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc)) # Automatically set the timestamp to the current time when a new TODO is created
