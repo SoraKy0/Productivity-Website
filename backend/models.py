b7969a442a8912a3f31fe5e520de34b95a2af167
SoraKy0 <154877077+SoraKy0@users.noreply.github.com>
2026-02-09 01:23:57 +0000

    defined TODO database model schema

--- backend/models.py
+++ backend/models.py
@@ -6,3 +6,13 @@ Goals:
 2. Define the "TODO" model with fields: id, title, and completed status.
 2. Ensure the database knows exactly what data to expect. 
 """
+from sqlmodel import SQLModel, Field
+from datetime import datetime, timezone
+
+class TODO(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str
+    description: str | None = Field(default=None, max_length=600)
+    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc)) # Automatically set the timestamp to the current time when a new TODO is created
+    position: int = Field(default=0) # Pos of TODO in list.
+    priority: int = Field(default=0) # There will be 3 levels of priority. (0 = No priority, 1 = Low, 2 = Medium, 3 = High)
