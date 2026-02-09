60b27e16c6a426f8fb857bdf3241716e97696e8f
SoraKy0 <154877077+SoraKy0@users.noreply.github.com>
2026-02-09 00:05:50 +0000

    Configured SQLite database engine and connection

--- backend/database.py
+++ backend/database.py
@@ -6,3 +6,11 @@ Goals:
 2. Configure how python talks to the database.
 3. Create a "Session" which is like a single transaction (opening the database, doing work, and closing it).
 """
+from sqlmodel import create_engine
+
+
+sqlite_file_name = "database.db" # Name of database file
+sqlite_url = f"sqlite:///{sqlite_file_name}" # Database URL for SQLModel
+
+connect_args = {"check_same_thread": False} # Configues SQLite to allow multiple threads to access the database (important for FastAPI)
+engine = create_engine(sqlite_url, connect_args=connect_args) # Create the engine that connects the code to the database
